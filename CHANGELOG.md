fa13b1b1522d667650989064601e90f07e6520e9
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2023-09-06 23:43:17 +0900

    upd: new changelog validator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,9 @@
 - New filled icon in bookmarks.
 - Fix button icons oversized.
 
-[Full Changelog](https://github.com/dracula/mixplorer/compare/v1.2.0...v1.3.0)
+**Full Changelog**: <https://github.com/dracula/mixplorer/compare/v1.2.0...v1.3.0>
+
+[1.3.0]: <https://github.com/dracula/mixplorer/tree/v1.3.0>
 
 ## [1.2.0] - 2023-07-22
 
@@ -20,22 +22,23 @@
 - New monocolor icon in bookmarks ([**#9**](https://github.com/dracula/mixplorer/issues/9))
 - Remove the `Hubic Drive` and `Amazon Cloud Drive` icons from the `Add storage` option as they may not be able to show the icons in MiXplorer _v6.60+_.
 
-[Full Changelog](https://github.com/dracula/mixplorer/compare/v1.1.0...v1.2.0)
+**Full Changelog**: <https://github.com/dracula/mixplorer/compare/v1.1.0...v1.2.0>
+
+[1.2.0]: <https://github.com/dracula/mixplorer/tree/v1.2.0>
 
 ## [1.1.0] - 2022-04-04
 
 - Fix selection background in code editor. ([**#4**](https://github.com/dracula/mixplorer/issues/4#issuecomment-968925140)) ([**800452a**](https://github.com/dracula/mixplorer/commit/800452ab1e30ddca52d93e4929f5543ab9c8e60f))
 - Some issues with the FTP and HTTP/WebDav tile icons in the curtain, thanks [@Pushkin31](https://github.com/Pushkin31) for solving it ([**f82d122**](https://github.com/dracula/mixplorer/pull/5/commits/f82d122)) closed ([**#4**](https://github.com/dracula/mixplorer/issues/4#issuecomment-952234665))
 
-[Full Changelog](https://github.com/dracula/mixplorer/compare/v1.0.0...v1.1.0)
+**Full Changelog**: <https://github.com/dracula/mixplorer/compare/v1.0.0...v1.1.0>
+
+[1.1.0]: <https://github.com/dracula/mixplorer/tree/v1.1.0>
 
 ## [1.0.0] - 2021-10-22
 
-[_Initial commits._](https://github.com/dracula/mixplorer/commits/v1.0.0)
+_Initial commits._
 
-[Full Changelog](https://github.com/dracula/mixplorer/compare/de55b20c7ca6c165236a03a08e8faa9d095084d9...v1.0.0)
+**Full Changelog**: <https://github.com/dracula/mixplorer/commits/v1.0.0>
 
-[1.3.0]: <https://github.com/dracula/mixplorer/tree/v1.3.0>
-[1.2.0]: <https://github.com/dracula/mixplorer/tree/v1.2.0>
-[1.1.0]: <https://github.com/dracula/mixplorer/tree/v1.1.0>
 [1.0.0]: <https://github.com/dracula/mixplorer/tree/v1.0.0>
--- scripts/changelog.ps1
+++ scripts/changelog.ps1
@@ -1,68 +1,115 @@
-[CmdletBinding(DefaultParameterSetName = 'WithHeader')]
+[CmdletBinding(DefaultParameterSetName = 'Header')]
 param (
-    # Save as output to file name.
-    [Parameter(ParameterSetName = 'WithHeader')]
-    [Parameter(ParameterSetName = 'NoWithHeader')]
-    [Alias('o')][string]$OutFile,
-    # Update header date to current date.
-    [Parameter(ParameterSetName = 'WithHeader')]
-    [Alias('u')][switch]$NowDate,
-    # Remove first header.
-    [Parameter(ParameterSetName = 'NoWithHeader')]
-    [Alias('n')][switch]$NoHeader
+    [Parameter(ParameterSetName = 'Header')]
+    [Alias('d')][int]$Depth = 0,
+    [Parameter(ParameterSetName = 'Header')]
+    [Alias('n')][switch]$NoHeader,
+    [Parameter(ParameterSetName = 'Example')]
+    [Alias('e')][switch]$Example
 )
 
-$ROOT_PATH = [System.IO.Path]::GetFullPath("$PSScriptRoot/..")
+function example_changelog {
+    'Add changes to CHANGELOG.md. For example:'; ''
+    Write-Host -ForegroundColor Cyan @'
+## [2.0.0] - 2020-02-20
+
+_If you are upgrading: please see [`UPGRADING.md`](UPGRADING.md)._
+
+### Removed
+
+- **Breaking:** remove `write()` method from public API (`01e3a64`)
+
+**Full Changelog**: <https://github.com/owner/repo/compare/v1.0.0...v2.0.0>
+
+[2.0.0]: <https://github.com/owner/repo/tree/v2.0.0>
+
+## [1.0.0] - 2010-01-10
+
+_First release._
+
+**Full Changelog**: <https://github.com/owner/repo/commits/v1.0.0>
+
+[1.0.0]: <https://github.com/owner/repo/tree/v1.0.0>
+'@
+    return
+}
+
+if ($Example) { return example_changelog }
+
+$srcFile = [System.IO.Path]::GetFullPath("$PSScriptRoot/../CHANGELOG.md")
+if (!([System.IO.File]::Exists($srcFile))) {
+    "File could not be found '$srcFile'."
+    example_changelog
+    exit 1
+}
+
+function latest_changelog {
+    [System.Text.RegularExpressions.Regex]::Matches(
+        [System.IO.File]::ReadAllText($srcFile),
+        [System.String]::new('^##[^#\n]+([\W\w]*?)\[([\d.]+)\]\:.+'),
+        [System.Text.RegularExpressions.RegexOptions]::Multiline
+    ).Value[$Depth]
+    return
+}
+
+function noheader_changelog {
+    $f = [System.IO.Path]::GetTempFileName()
+    [System.IO.File]::WriteAllText($f, $(latest_changelog))
+    $l = [System.IO.File]::ReadAllLines($f)
+    [System.IO.File]::Delete($f)
+    $h = $l -match '^##\s\[([\d.]+)\]\s-\s\d{4}-\d{2}-\d{2}'
+    $e = $l -match '\[([\d.]+)\]\:'
+    $w = [System.Collections.Generic.List[string]]::new()
+    foreach ($i in $l) {
+        if ($i.Contains($h)) { continue };
+        if ($i.Contains($e)) { break };
+        $w.Add($i)
+    }
+    if ($w[0].Length -eq 0) { $w.RemoveAt(0) }
+    if ($w[-1].Length -eq 0) { $w.RemoveAt($w.Count - 1) }
+    return $w
+}
+
+if ($NoHeader) {
+    return noheader_changelog
+} else {
+    return latest_changelog
+}
+
+# OLDER
+<# $ROOT_PATH = [System.IO.Path]::GetFullPath("$PSScriptRoot/..")
 $inFileChangelog = [System.IO.Path]::Combine($ROOT_PATH, 'CHANGELOG.md')
 if (!([System.IO.File]::Exists($inFileChangelog))) {
     "File could not be found '$inFileChangelog'."
     exit 1
 }
 
-if ($OutFile -and $OutFile.EndsWith('CHANGELOG.md')) {
-    "File cannot be the same as '$inFileChangelog'."
-    exit 1
-}
-
 [System.Console]::InputEncoding = [System.Console]::OutputEncoding = $OutputEncoding = [System.Text.UTF8Encoding]::new()
-$readLines = [System.IO.File]::ReadAllLines($inFileChangelog).Trim()
-$headings = $readLines -match '^##.\[([\d.]+)\].-.\d{4}-\d{2}-\d{2}'
-$footers = $readLines -match '\[Full Changelog\]'
-$headDefs = $readLines -match '\[([\d.]+)\]\:'
+$pattern = @{ head = '^##\s\[([\d.]+)\]\s-\s\d{4}-\d{2}-\d{2}'; date = '\d{4}-\d{2}-\d{2}'; link = '\[([\d.]+)\]\:' }
+$readLines = [System.IO.File]::ReadAllLines($inFileChangelog)
+$headings = $readLines -match $pattern.head
+$headLink = $readLines -match $pattern.link
 $currentDate = [string]::Format('{0:yyyy-MM-dd}', [datetime]::Now)
 
-$startHeader = $headings[0]
-
-if ($startHeader) {
+if ($headings[0] -match $pattern.head) {
     $writeLines = [System.Collections.Generic.List[string]]::new()
     foreach ($line in $readLines) {
-        $line = $line.Trim()
-        if ($line.Contains($headings[1])) { break };
-        if ($line.Contains($footers[0])) { $writeLines.Add($line); break }
-        if ($line.Contains($startHeader) -and $NowDate) {
-            $semanticVersion = $startHeader.Split(' ')[1].Trim('[]')
-            $newStartHeader = "## [$semanticVersion] - $currentDate"
-            $line = $line -replace $startHeader, $newStartHeader
+        if ($line.Contains($headings[1])) { break }
+        if ($line.Contains($headings[0]) -and $NowDate) {
+            $line = $line -replace $pattern.date, $currentDate
         }
         $writeLines.Add($line)
     }
+    if ($writeLines[-1].Length -eq 0) { $writeLines.RemoveAt($writeLines.Count - 1) }
     if ($NoHeader) {
-        if ($writeLines[0].Contains($startHeader)) { $writeLines.RemoveAt(0) }
+        if ($writeLines[0].Contains($headings[0])) { $writeLines.RemoveAt(0) }
         if ($writeLines[0].Length -eq 0) { $writeLines.RemoveAt(0) }
-    } elseif ($headDefs[0]) {
-        $writeLines.Add($null); $writeLines.Add($headDefs[0])
+    } elseif ($headLink[0] -match $pattern.link) {
+        $writeLines.Add($null); $writeLines.Add($headLink[0])
     }
-    if ($OutFile) {
-        $DirPath = [System.IO.Path]::GetDirectoryName([System.IO.Path]::GetFullPath($OutFile))
-        if (!([System.IO.Directory]::Exists($DirPath))) {
-            [System.IO.Directory]::CreateDirectory($DirPath) | Out-Null
-        }
-        [System.IO.File]::WriteAllLines($OutFile, $writeLines)
-    } else {
-        return $writeLines
-    }
+    return $writeLines
 } else {
     "Mismatched heading like '## [semver] - yyyy-MM-dd'."
     "example heading: ## [1.0.0] - $currentDate"
     exit 1
-}
+} #>
