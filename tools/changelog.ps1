b10eb8b7656a02cea21c28f99d0ae4e7bb1a718a
Andre Sanjaya <73837775+sionta@users.noreply.github.com>
2023-11-15 03:37:04 +0900

    dc4df97f

--- tools/changelog.ps1
+++ tools/changelog.ps1
@@ -7,9 +7,41 @@ param (
     [Parameter(ParameterSetName = 'sample')]
     [Alias('e')][switch]$Example
 )
-begin {
-    $examples = @'
-<!-- BEGIN -->
+
+if ($Example) { return $examples }
+
+# [System.Console]::InputEncoding = [System.Console]::OutputEncoding = $OutputEncoding = [System.Text.UTF8Encoding]::new()
+$srcFile = [System.IO.Path]::GetFullPath("$PSScriptRoot/../CHANGELOG.md")
+if ([System.IO.File]::Exists($srcFile)) {
+    $results = [System.Text.RegularExpressions.Regex]::Matches(
+        [System.IO.File]::ReadAllText($srcFile),
+        [string]'^##\s\[\d.+\][\n\W\w]*?\[[\d.]+\]\:.*[^\n]',
+        [System.Text.RegularExpressions.RegexOptions]::Multiline
+    ).Value[$Index]
+} else {
+    Write-Error "File could not be found '$srcFile'."
+}
+
+if ($results) {
+    $mlines = $results -split "`n"
+    $mlines = $mlines -notmatch '\<\!-[\W\w]*?-\>'    # <!-- comments -->
+    $header = $mlines -match '^##\s\[([\d.]+)\]'       # ## [semver] - yyyy-mm-dd
+    $footer = $mlines -match '^\[([\d.]+)\]\:'         # [semver]: <url>
+    if ($header[0] -and $footer[0]) {
+        foreach ($line in $mlines) {
+            if ($line -eq $header[1]) { break }
+            if ($line -eq $footer[0] -and $NoHeader) { break }
+            if ($line -eq $header[0] -and $NoHeader) { continue }
+            Write-Host $line
+        }
+    } else {
+        Write-Error "Incorrect syntax, for example:`n"
+        Write-Host $examples -ForegroundColor Green
+        exit 1
+    }
+}
+
+$examples = @'
 ## [1.3.0] - 2023-09-03
 
 ### Added
@@ -17,12 +49,13 @@ begin {
 - New `Purple` accent color.
 - New filled icon in bookmarks.
 
+<!-- Markdown comment -->
+
 ### Fixed
 
 - Button icons oversized.
 
 [1.3.0]: <https://github.com/dracula/mixplorer/compare/v1.2.0...v1.3.0>
-<!-- ENDED -->
 
 ## [1.0.0] - 2021-10-22
 
@@ -30,50 +63,6 @@ _Initial commits._
 
 [1.0.0]: <https://github.com/dracula/mixplorer/commits/v1.0.0>
 '@
-    if ($Example) { return $examples }
-    # [System.Console]::InputEncoding = [System.Console]::OutputEncoding = $OutputEncoding = [System.Text.UTF8Encoding]::new()
-    $srcFile = [System.IO.Path]::GetFullPath("$PSScriptRoot/../CHANGELOG.md")
-    if ([System.IO.File]::Exists($srcFile)) {
-        $results = [System.Text.RegularExpressions.Regex]::Matches(
-            [System.IO.File]::ReadAllText($srcFile),
-            [string]'^##\s\[\d.+\][\n\W\w]*?\[[\d.]+\]\:.*[^\n]',
-            [System.Text.RegularExpressions.RegexOptions]::Multiline
-        ).Value[$Index]
-    } else {
-        "File could not be found '$srcFile'."
-    }
-}
-process {
-    if ($results) {
-        $temp = [System.IO.Path]::GetTempFileName()
-        [System.IO.File]::WriteAllText($temp, $results)
-        $lines = [System.IO.File]::ReadAllLines($temp)
-        [System.IO.File]::Delete($temp)
-        $lines = $lines -notmatch '\<\!-[\W\w]*?-\>' # <!-- comments -->
-        $head = $lines -match '^##\s\[([\d.]+)\]'    # ## [semver] - yyyy-mm-dd
-        $foot = $lines -match '^\[([\d.]+)\]\:'      # [semver]: <url>
-        if ($head[0] -and $foot[0]) {
-            $words = [System.Collections.Generic.List[string[]]]::new()
-            foreach ($line in $lines) {
-                if ($line -eq $head[1]) { break }
-                if ($line -eq $foot[0] -and $NoHeader) { break }
-                if ($line -eq $head[0] -and $NoHeader) { continue }
-                $words.Add($line)
-            }
-        } else {
-            "Incorrect syntax, for example:"
-            $examples
-        }
-    }
-}
-end {
-    if ($words) {
-        if (!$words[0]) { $words.RemoveAt(0) }
-        if (!$words[-1]) { $words.RemoveAt($words.Count - 1) }
-        $words
-    }
-    return
-}
 
 <# DUMMY REGEX
     # see: https://stackoverflow.com/a/42681472
--- tools/changelog.py
+++ tools/changelog.py
@@ -1,41 +1,64 @@
-import os
 import re
-import argparse
-from datetime import datetime
 
-def display_changelog(noheader):
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    changelog_file = os.path.join(script_directory, '..', 'CHANGELOG.md')
-    try:
-        if not os.path.isfile(changelog_file):
-            raise FileNotFoundError(f"File not found: '{changelog_file}'")
-        with open(changelog_file, 'r') as file:
-            changelog = file.read()
-        pattern = r"## \[([^]]+)\]\n\n(.*?)\n\n\[\1\]: <([^>]+)>"
-        matches = re.findall(pattern, changelog, re.DOTALL)
-        for match in matches:
-            version = match[0]
-            url = match[2]
-            if not args.noheader and re.match(r'## \[\d+\.\d+\.\d+\]', f'## [{version}]'):
-                current_date = datetime.now().date().strftime('%Y-%m-%d')
-                print(f'[{version}] - {current_date}\n')
-            content = match[1]
-            if args.noheader and content:
-                print(f'{content}\n')
-                break
-            else:
-                if content:
-                    print(f'{content}\n')
-            if re.match(r'\[\d+\.\d+\.\d+\]: <[^>]+>', f'[{version}]: <{url}>'):
-                print(f'[{version}]: <{url}>\n')
-                break
-    except FileNotFoundError as e:
-        print(e)
-    except Exception as e:
-        print(f"Error: {e}")
+sample_text = """
+# Changelog
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Changelog Extraction')
-    parser.add_argument('-n', '--noheader', action='store_true', help='Don\'t display header')
-    args = parser.parse_args()
-    display_changelog(args.noheader)
+## [Unreleased]
+
+[unreleased]: https://github.com/dracula/mixplorer/compare/v1.3.0...HEAD
+
+## [1.3.0]
+
+<!-- BEGIN -->
+
+- New `Purple` accent color.
+- New filled icon in bookmarks.
+- Fix button icons oversized.
+
+[1.3.0]: <https://github.com/dracula/mixplorer/compare/v1.2.0...v1.3.0>
+
+## [1.2.0]
+
+- Add `Dracula` logo in skin settings.
+- Add `Shuffle` icon in sort menu (_v6.60+_).
+- Add `No item` icon if folder is empty.
+- Add new icon in bookmarks ([**#10**](https://github.com/dracula/mixplorer/issues/10)):
+  - `Instagram`
+  - `Telegram`
+  - `Twitter`
+  - `WhatsApp`
+  - `Viber`
+- New monocolor icon in bookmarks ([**#9**](https://github.com/dracula/mixplorer/issues/9))
+- Remove the `Hubic Drive` and `Amazon Cloud Drive` icons from the `Add storage` option as they may not be able to show the icons in MiXplorer _v6.60+_.
+
+[1.2.0]: <https://github.com/dracula/mixplorer/compare/v1.1.0...v1.2.0>
+
+## [1.1.0]
+
+- Fix selection background in code editor. ([**#4**](https://github.com/dracula/mixplorer/issues/4#issuecomment-968925140)) ([**800452a**](https://github.com/dracula/mixplorer/commit/800452ab1e30ddca52d93e4929f5543ab9c8e60f))
+- Some issues with the FTP and HTTP/WebDav tile icons in the curtain, thanks [@Pushkin31](https://github.com/Pushkin31) for solving it ([**f82d122**](https://github.com/dracula/mixplorer/pull/5/commits/f82d122)) closed ([**#4**](https://github.com/dracula/mixplorer/issues/4#issuecomment-952234665))
+
+[1.1.0]: <https://github.com/dracula/mixplorer/compare/v1.0.0...v1.1.0>
+
+## [1.0.0]
+
+_Initial commits._
+
+[1.0.0]: <https://github.com/dracula/mixplorer/commits/v1.0.0>
+"""
+
+# Applying the regex pattern with re.MULTILINE flag
+pattern = r'^##\s\[\d.+\][\n\W\w]*?\[[\d.]+\]\:.*[^\n]'
+matches = re.findall(pattern, sample_text, re.MULTILINE)[0]
+for line in matches:
+    line = line.splitlines()
+    if re.search(r'^##\s\[\d+\.\d+\.\d+\]', line):
+        # Skip lines matching version header pattern
+        continue # ## [version] - yyyy-mm-dd
+    if re.search(r'<!--[\s\S]*?-->', line):
+        # Skip lines containing comments
+        continue # <!-- comments -->
+    if re.search(r'^\[\d+\.\d+\.\d+\]\:', line):
+        # Skip lines matching version footer pattern
+        continue # [version]: <url>
+    print(line)
