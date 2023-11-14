40658433c890aa09d5174f812a4f2e445c4b69ba
Andre Sanjaya <73837775+sionta@users.noreply.github.com>
2023-11-15 01:28:27 +0900

    438025f5

--- tools/test_release.ps1
+++ tools/test_release.ps1
@@ -1,47 +1,73 @@
 param(
-    [string]$tagName,
-    [switch]$All
+    [string][alias('t')]$tagName,
+    [switch][alias('d')]$deleteAllTag,
+    [switch][alias('a')]$pushAddAll
 )
 
-foreach ($tool in 'git','gh') {
+# ./tools/test_release.ps1 'v1.3.0'
+
+foreach ($tool in 'git', 'gh') {
     if (-not(Get-Command $tool -ea:0)) {
         Write-Host "Please install: $tool."
         exit 1
     }
 }
 
-# eg. ./tools/test_release.ps1 'v1.3.0'
+$REPOSITORY = 'sionta/mixplorer'
 
-$repository = 'sionta/mixplorer'
+function delete_release($tag) {
+    if (-not $tag) { $tag = $tagName }
+    $release = gh api repos/$REPOSITORY/releases | ConvertFrom-Json
+    if ($release.tag_name -eq $tagName) {
+        gh release delete $tagName --repo $REPOSITORY --yes --cleanup-tag
+    }
+}
 
-if ($All) {
+function delete_tag_name {
+    # delete tag release
+    delete_release
+    # delete tag locally
+    $tagLocal = git tag -l | Where-Object { $_ -eq $tagName }
+    if ($tagLocal) { git tag -d $tagName }
+    # delete tag remotely
+    $tagRemote = git ls-remote --tags origin $tagName
+    if ($tagRemote) { git push origin --delete $tagName }
+}
+
+function create_tag_name {
+    delete_tag_name $tagName
+    $tagMessage = "Release version $($tagName -replace 'v','')"
+    git tag -a $tagName -m $tagMessage
+    git push origin $tagName
+}
+
+
+
+function push_add_all {
+    $message = $([System.Guid]::NewGuid()).ToString().Split('-')[0]
+    $branch = git rev-parse --abbrev-ref HEAD
+    git add .
+    git commit -m $message
+    push origin $branch
+}
+
+function delete_all_tag {
+    # delete all tag locally
     git tag | ForEach-Object { git tag -d $_ }
+    # delete all tag remotely
     git ls-remote --tags origin | ForEach-Object {
         $remoteTag = $_.Split('/')[-1]
+        # delete all releases
+        delete_release $remoteTag
         git push origin --delete $remoteTag
-        gh release delete $remoteTag --repo $repository --yes --cleanup-tag
     }
     return
 }
 
-# $apiGithub = gh api repos/$repository/releases | ConvertFrom-Json
-gh release delete $tagName --repo $repository --yes --cleanup-tag
+if ($pushAddAll) { return push_add_all }
+if ($deleteAllTag) { return delete_all_tag }
 
-$tagMessage = "Release version $($tagName -replace 'v','')"
-$commitMessage = $([System.Guid]::NewGuid()).ToString().Split('-')[0]
-$currentBranch = git rev-parse --abbrev-ref HEAD
-
-# delete tag locally
-$tagLocally = git tag -l | Where-Object { $_ -eq $tagName }
-if ($tagLocally) { git tag -d $tagName }
-
-# delete tag remotely
-$tagRemotely = git ls-remote --tags origin $tagName
-if ($tagRemotely) {git push origin --delete $tagName}
-
-# push commit all and push tag name
-git add .
-git commit -m $commitMessage
-git push origin $currentBranch
-git tag -a $tagName -m $tagMessage
-git push origin $tagName
+push_add_all
+delete_release
+delete_tag_name
+create_tag_name
